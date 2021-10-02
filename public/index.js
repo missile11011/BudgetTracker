1c8a029cf81550216c750264b807336cc1c01f38
Misael Reyes <misaelrey90@gmail.com>
2021-10-01 22:04:16 -0500

    service-workers work offline

--- public/index.js
+++ public/index.js
@@ -136,7 +136,8 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
-    saveRecord(transaction);
+    // saveRecord(transaction);
+    DatabaseStore.add(transaction)
 
     // clear form
     nameEl.value = "";
--- public/service-worker.js
+++ public/service-worker.js
@@ -11,12 +11,13 @@ const FILES_TO_CACHE = [
 ];
 self.addEventListener("install", function (evt) {
 	evt.waitUntil(
-		caches.open(DATA_CACHE_NAME).then((cache) => cache.add("./icons"))
+		caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
 	);
 
 	evt.waitUntil(
 		caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
 	);
+    self.skipWaiting();
 });
 self.addEventListener("activate", function (evt) {
 	evt.waitUntil(
